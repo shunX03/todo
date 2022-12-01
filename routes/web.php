d112ce531f2b3f48de297b3c91e23e01b08b5d92
EC2 Default User <ec2-user@ip-172-31-7-241.us-west-1.compute.internal>
2022-12-01 14:55:43 +0000

    create mvc

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TodoFormController@index');
+Route::get('/create-page', 'TodoFormController@createPage');
+Route::post('/create', 'TodoFormController@create');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
